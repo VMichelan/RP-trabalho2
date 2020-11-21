07567cc4fcbce6e9e4c1c40c7c00475af1abad78
VMichelan <venanciuslm@gmail.com>
2020-11-21 12:41:24 -0300

    Faz a média do welch para todos os eletrodos

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ eeg_bands = {
         'Gama' : (30    , 100)
 }
 
-window_size = 3
+window_size = 5
 chunk_size = 250
 
 def main():
@@ -39,37 +39,40 @@ def main():
     inlet = StreamInlet(resolve_stream('type', 'EEG')[0])
 
     chunk, timestamp = inlet.pull_chunk(timeout=3, max_samples=chunk_size)
-    chunk = np.array(chunk).flatten()
-    buffer = np.array(chunk)
+    buffer = np.array(chunk).transpose()
 
     print("first buffer", buffer.shape, buffer)
 
     print("Filling buffer")
     for i in range(window_size - 2):
         chunk, timestamp = inlet.pull_chunk(timeout=3, max_samples=chunk_size)
-        chunk = np.array(chunk).flatten()
-        buffer = np.concatenate((buffer, chunk))
+        chunk = np.array(chunk).transpose()
+        buffer = np.hstack((buffer, chunk))
         print(i)
 
     print(buffer.shape)
     print("Processing")
     while True:
         chunk, timestamp = inlet.pull_chunk(timeout=3, max_samples=chunk_size)
-        chunk = np.array(chunk).flatten()
-        chunk = chunk.flatten()
+        chunk = np.array(chunk).transpose()
 
-        buffer = np.concatenate((buffer, chunk))
+        buffer = np.hstack((buffer, chunk))
         print("buffer", buffer.shape, buffer)
 
         freqs, psd = welch(buffer, fs=250., nperseg=4*250)
+        psd = np.average(psd, axis=0);
+        print("freqs", freqs.shape, freqs)
+        print("psd", psd.shape, psd)
 
         freq_res = freqs[1] - freqs[0] 
+        print("freq_res", freq_res)
 
         total_power = simps(psd, dx=freq_res)
 
         band_power = []
         for band in eeg_bands:
             idx = np.logical_and(freqs >= eeg_bands[band][0], freqs <= eeg_bands[band][1])
+            print("idx", idx.shape, idx)
             # print("min:", eeg_bands[band][0], "max:", eeg_bands[band][1])
             # print(freqs >= eeg_bands[band][0])
             # print(freqs <= eeg_bands[band][1])
@@ -88,7 +91,7 @@ def main():
         print(band_power)
 
         print(buffer.shape)
-        buffer = np.delete(buffer, slice(chunk_size * 8), axis=0)
+        buffer = np.delete(buffer, slice(250), axis=1)
         print(buffer.shape)
 
 if __name__ == '__main__':
